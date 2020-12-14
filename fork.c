84652c6a574835ae21bbe6f11c8d2230208a226c
Nallamilli S V S R Krushik Reddy <52321852+KrushikReddyNallamilli@users.noreply.github.com>
2020-12-14 10:53:51 +0530

    Update fork.c

--- fork.c
+++ fork.c
@@ -35,3 +35,24 @@ int main()
 		printf("\n Parent: my child is dead with pid %d and exit status %d, I am going to leave, bye. \n",id,s);
 	}
 }
+
+
+// Output:
+
+//  Parent: mypid is 1931 
+
+//  Parent: my parrent's pid is 1791 
+
+//  Parent: I created a child process
+//  Parent: My child's pid is 1932 
+
+//  child: I an a new born process 
+
+//  Child: mypid is 1932 
+
+//  Child: my parrent's pid is 1931 
+
+//  Child: I am executing date command 
+// Mon Dec 14 10:49:48 IST 2020
+
+//  Parent: my child is dead with pid 1932 and exit status 0, I am going to leave, bye. 
